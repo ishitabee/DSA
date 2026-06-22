3e4ab45477c31c03d0b6e10bb4de213aea30282e
ishitabee <ishitabiswal333@gmail.com>
2026-06-22 13:40:08 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -10,7 +10,7 @@ public class InsertionSort {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        char choice;
+        char choice = " ";
 
         do {
 
