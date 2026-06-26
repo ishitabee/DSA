aa18d9d464348298a3ac252bd15f0518d1392f3e
Ishita <ishitabiswal333@gmail.com>
2026-06-26 16:19:47 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,82 @@
+
+class Node {
+    int data;
+    Node next;
+
+    Node (int data){
+        this.data = data;
+        this.next = null;
+    
+    }
+}
+
+public class Stack {
+
+    Node top;
+
+    //push node
+    void pushNode(int data){
+        Node newNode = new Node(data);
+
+        newNode.next = top;
+
+        top = newNode;
+
+        System.out.println(data + " pushed into stack.");
+    }
+
+    //pop node
+    void popNode()
+    {
+        if(top == null)
+        {
+            System.out.println("Nothing can be popped from an empty stack.");
+            return;
+        }
+
+        System.out.println("Element " + top.data + " will be popped.");
+
+        top = top.next;
+    }
+
+    //display stack
+    void displayStack()
+    {
+        if(top == null)
+        {
+            System.out.println("Stack is empty.");
+            return;
+        }
+
+        Node temp = top;
+
+        while(temp != null)
+        {
+            System.out.println(temp.data);
+
+            temp = temp.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        Stack myStack = new Stack();
+
+        //pushing nodes into a stack
+        myStack.pushNode(1);
+        myStack.pushNode(16);
+        myStack.pushNode(22);
+        myStack.pushNode(30);
+
+        //displaying the newly created stack
+        System.out.println("\nStack: ");
+        myStack.displayStack();
+
+        //popping node from the stack. Only topmost/last node gets popped out.
+        myStack.popNode();
+        System.out.println("\nStack after popping the node: ");
+        myStack.displayStack();
+    }
+
+}
